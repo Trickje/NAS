67d7cc5a3b15c37518141999579f6d2b6a8a8973
Trickje <rick.pijpers@hotmail.com>
2024-01-28 16:54:25 +0100

    Fixed the test

--- test/arrayindex.test.js
+++ test/arrayindex.test.js
@@ -5,7 +5,7 @@ import { expect } from "chai";
 describe("Array", function () {
   describe("#indexOf()", function () {
     it("should return -1 when the value is not present", function () {
-      expect([1, 2, 3].indexOf(4)).to.equal(1); //I changed this to intentionally break the tests.
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
 });
