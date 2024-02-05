b4dbc766e26f78a7b653ddcd9d4c8d0ae4160f66
Trickje <rick.pijpers@hotmail.com>
2024-02-05 07:02:48 +0100

    Added a partial function "can_be_new_password"

--- modules/check_password.js
+++ modules/check_password.js
@@ -1,3 +1,4 @@
+import { compare_password } from "./hash_password.js";
 export function check_password(password) {
   /**
    * This regex enforces the following rules for the password:
@@ -10,4 +11,20 @@ export function check_password(password) {
   const checking_regex =
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])(?!.*[="'\`])[A-Za-z\d@$!%*?&]{8,}$/;
   return checking_regex.test(password);
+}
+
+export function can_be_new_password(password) {
+  //TODO: Call to the database and receives a hashed password
+  const previousPass = "";
+  if (compare_password(password, previousPass)) {
+    //Cannot be the same a the previous password.'
+    return false;
+  }
+
+  if (check_password(password) == false) {
+    //Should follow the password guidelines
+    return false;
+  }
+
+  return true;
 }
