3828b6930649ed23d114e1de5b1206f5f83a070d
Trickje <rick.pijpers@hotmail.com>
2024-02-19 07:40:14 +0100

    Linked the register page to a register button and made the register page in the same style as the login page.

--- modules/create_server.js
+++ modules/create_server.js
@@ -52,6 +52,47 @@ function serveLoginPage(req, res, errorMsg) {
   });
 }
 
+function serveRegisterPage(req, res, errorMsg) {
+  let filePath = "." + req.url;
+  filePath = "./pages/register.html";
+
+  const extname = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404);
+        res.end("404 Not Found");
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+}
+
 // Function to handle POST request for login
 function handleLogin(req, res) {
   let body = "";
@@ -88,6 +129,8 @@ export function create_server(req, res) {
     serveLoginPage(req, res);
   } else if (req.method === "POST" && req.url === "/login") {
     handleLogin(req, res);
+  } else if (req.url === "/register") {
+    serveRegisterPage(req, res);
   }
   // Handle other routes
   else {
